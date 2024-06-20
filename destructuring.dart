525bc2c34497e2ef8ce0bc506632a2881006b289
devanarayanansb <devan20034014@gmail.com>
2024-06-20 09:09:20 +0530

    new

---
+++ destructuring.dart
@@ -0,0 +1,5 @@
+void main(){
+  List<int>list=[1,2,3,4];
+  final[a,b,c,d]=list;
+  print(list);
+}
--- list.dart
+++ list.dart
@@ -1,6 +1,6 @@
 void main(){
   List<int> list=[2,3,8,5];
-  List<int> list2 =list;
+  List<int> list2 =[...list,33,44];
   list[0]=12;
   print(list);
   print(list2);
