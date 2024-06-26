6ce9898f171db9b9e892c226ebc919b0f03ada18
Devanarayanan.S.B <devan20034014@gmail.com>
2024-06-26 08:39:42 +0530

    new
    upper and 5fns

---
+++ uplower.dart
@@ -0,0 +1,10 @@
+void main() {
+  String x = "Deva narayanan";
+  print(x);
+  print(x.toUpperCase());
+  print(x.toLowerCase());
+print(x.trim());
+print(x.trimLeft());
+print(x.trimRight());
+print(x.lastIndexOf("a"));
+}
