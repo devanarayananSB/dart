7ea448974f8c3765a3050561ff225cc4442c7d21
Devanarayanan.S.B <devan20034014@gmail.com>
2024-07-02 07:52:36 +0530

    work
    duplicate filterout

--- work9.dart
+++ work9.dart
@@ -1,18 +1,5 @@
 void main() {
-  String str = "hello i love apple very much too";
-  List<String> words = str.split(" ");
-  List<String> filteredWords = words.where((word) {
-    var charCount = <String, int>{};
-    for (var char in word.split("")) {
-      if (charCount.containsKey(char)) {
-        charCount[char] = charCount[char]! + 1;
-      } else {
-        charCount[char] = 1;
-      }
-    }
-    return charCount.values.every((count) => count == 1);
-  }).toList();
-
-  String result = filteredWords.join(" ");
-  print(result);
+  String x = "i eat apple";
+  final y = x.split(" ").where((i) => i.split("").toSet().length == i.length);
+  print(y);
 }
