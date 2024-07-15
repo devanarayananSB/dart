72cbd995622633be0d43b2aee9ee43158d4fdfd3
Devanarayanan.S.B <devan20034014@gmail.com>
2024-07-15 08:54:50 +0530

    work
    class work

---
+++ synfwork.dart
@@ -0,0 +1,51 @@
+/*SUM___*/
+//void main() {
+//int n=365;
+// int sum = 0;
+//while (n > 0) {
+// int m = n % 10;
+//sum = sum + m;
+//n=n~/10;
+//}
+//print("$sum");
+//}
+
+//FIZZBUZZ____
+//void main() {
+//for (int i = 1; i<= 100; i++) {
+//if (i % 3 == 0 && i % 5 == 0) {
+// print("FizzBuzz");
+//} else if (i % 3 == 0) {
+//print("Fizz");
+//} else if (i % 5 == 0) {
+// print("Buzz");
+//      } else {
+//print(i);
+// }
+//}
+//}
+
+/*Reverse
+void main() {
+  int n = 365;
+  int rev = 0;
+  while(n!=0){
+    final rm=n%10;
+    rev=rev*10+rm;
+    n=n~/10;
+    }
+    print(rev);
+    
+  }
+  */
+
+/*void main() {
+  for (int i = 1; i <= 100; i++) {
+    if ((i&1)==0){
+      print("$i is even");
+      }else{
+        print("$i is odd");
+        }
+  }
+    }
+  */
